de66e245ec08c08aeccf36839f6a7f52bd531601
Amos-jaf <araunahamos@gmail.com>
2023-10-06 05:46:51 +0100

    print without using printf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'\n");
+	return (1);
+}
