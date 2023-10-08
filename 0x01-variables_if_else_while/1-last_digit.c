18c3ccf6656fddc42fb5576a60766dd36b410a3d
Amos-jaf <araunahamos@gmail.com>
2023-10-08 14:01:10 +0000

    Print and qualify last digit of random number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,14 +14,14 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last_digit = n % 10;
-	if (last_digit > 5)
+	if (last_digit == 0)
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
+	else if (last_digit > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
-	else if (last_digit > 0 && last_digit < 6)
+	else if (last_digit != 0 && last_digit < 6)
 	{
 		printf("Last digit of %d is %d", n, last_digit);
 		printf(" and is less than 6 and not 0\n");
 	}
-	else
-		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	return (0);
 }
