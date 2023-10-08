4f853e18fe4a9e9c71471097fe89311fff344a22
Amos-jaf <araunahamos@gmail.com>
2023-10-08 18:39:49 +0000

    print digits of the Hex system

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Print all the digits of the hexadecimal system
+ * Description: Use first loop to iterate throught the numbers 0-9
+ * then use the second loop to iterate from a-f
+ * Return: (0) for success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	       putchar(i);
+	for (i = 'a'; i <= 'f'; i++)
+		putchar(i);
+	putchar('\n');	
+	return (0);
+}
