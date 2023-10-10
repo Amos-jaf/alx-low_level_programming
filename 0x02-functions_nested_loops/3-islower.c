84032c3af237ffbf90af5252b1a8c922d222d0c0
Amos-jaf <araunahamos@gmail.com>
2023-10-10 17:26:51 +0000

    check for lower and upper case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower(int c) - function returns 1 or 0 for lower case letter or not
+ * @c: integer for lower or upper case
+ * Description: If the argument c is lower case the function returns 1
+ * Return: (1) for lower case
+ */
+
+int _islower(int c)
+{
+	if (c > 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
