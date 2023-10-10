591fab9558367cc0ad925d9853f61dfd601e6c51
Amos-jaf <araunahamos@gmail.com>
2023-10-10 12:39:11 +0000

    printing putchar using custom function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * main - function prints strings
+ * Description: The function utilzes a custom putchar to print strings
+ * Return: (0)
+ */
+
+int main(void)
+{
+	_putchar('_');	
+	_putchar('p');	
+	_putchar('u');	
+	_putchar('t');	
+	_putchar('c');	
+	_putchar('h');	
+	_putchar('a');	
+	_putchar('r');	
+	_putchar('\n');	
+	return (0);
+}
