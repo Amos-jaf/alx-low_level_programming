f8ef62f30e46cf617922a8e98e0cd811f6806c5a
Amos-jaf <araunahamos@gmail.com>
2023-10-12 05:26:46 +0000

    print the multiplication tables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -28,20 +28,3 @@ void print_times_table(int n)
 	}
 	}
 }
-
-int main(void)
-{
-    print_times_table(3);
-    _putchar('\n');
-    print_times_table(5);
-    _putchar('\n');
-    print_times_table(98);
-    _putchar('\n');
-    print_times_table(12);  
-    return (0);
-}
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
