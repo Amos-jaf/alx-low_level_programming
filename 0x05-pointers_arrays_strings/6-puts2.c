571f314f4039e01f586c7e58cf44c1381af4f5ef
Amos-jaf <araunahamos@gmail.com>
2023-10-16 13:36:57 +0000

    print alternate characters in a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,11 +11,11 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == '\0')
+		if (i % 2 == 1)
 		{
-			break;
+			continue;
 		}
 		else
 		{
