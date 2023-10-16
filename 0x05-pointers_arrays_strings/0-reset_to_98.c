a867745937d5018ac80c9b7c2c68b6327fd431c0
Amos-jaf <araunahamos@gmail.com>
2023-10-16 06:54:34 +0000

    function that sets the value of a variable

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that changes the value of a variable
+ * @n: pointer to variable
+ * Description: Function uses pointer to change value of variable.
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
