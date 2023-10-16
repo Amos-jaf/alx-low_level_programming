eddbc7c8bb97dccca134bc7a5f755339c7c7a41e
Amos-jaf <araunahamos@gmail.com>
2023-10-16 13:26:58 +0000

    print alternate characters in a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - function prints alternate characters starting from the first
+ * @str: input string
+ * Description: Prints alternate characters using loop
+ * Return: (void)
+ */
+
+void puts2(char *str)
+{
+	int i;
+	for (i = 0; str[i] != '\0'; i+=2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
