cbc8759e3e2f75e1f46c626d61f8df5d42c695dc
Amos-jaf <araunahamos@gmail.com>
2023-10-18 11:26:25 +0000

    string concatenation

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - function to concatenate two strings
+ * @dest: final concatenated string
+ * @src: string added to destination string
+ * Description: The function add src string to destination string
+ * Return: (dest)
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int end, i, j;
+
+	j = 0;
+	for (end = 0; dest[end] != '\0'; end++)
+	{
+		continue;
+	}
+	for (i = end - 1; src[j] != '\0'; i++)
+	{
+		dest[i] = src[j];
+		j++;
+	}
+	return (dest);
+}
