aa694d3ad1d93250838932cae40abc2ac0405af6
Amos-jaf <araunahamos@gmail.com>
2023-10-23 15:58:59 +0000

    function that returns first occurence of a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strchr - function that returns a pointer to the first occurence of a string
+ * @s: input string
+ * @c: The charater to be found
+ * Description: Loops through the input string and searches for the first
+ * character c
+ * Return: (s) for success and NULL if not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	int check;
+
+	check = 0;
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			check = 1;
+			break;
+		}
+		s++;
+	}
+	if (check)
+	{
+		return (s);
+	}
+	else
+		return (NULL);
+}
