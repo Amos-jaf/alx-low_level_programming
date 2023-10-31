e9388c1d039a4216177794ae4f58d3c056caa730
Amos-jaf <araunahamos@gmail.com>
2023-10-31 14:50:24 +0000

    using malloc to for string concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -76,18 +76,3 @@ int str_count(char *s1, char *s2)
 	}
 	return (count);
 }
-
-int main(void)
-{
-    char *s;
-
-    s = str_concat("Betty ", "Holberton");
-    if (s == NULL)
-    {
-        printf("failed\n");
-        return (1);
-    }
-    printf("%s\n", s);
-    free(s);
-    return (0);
-}
