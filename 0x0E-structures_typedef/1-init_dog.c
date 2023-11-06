ebbbc806d6f55d2411c8db5920904d6bc6ec60dc
Amos-jaf <araunahamos@gmail.com>
2023-11-06 18:10:17 +0000

    function to create structure

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * init_dog - function to initilize struct dog
+ * @d: the name of the struct
+ * @name: pointer to name
+ * @age: age of the dog
+ * @owner: the owner of the dog
+ * Description: The function initializes a struct when the function is
+ * called with the following parameter
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - structure to declare propertes of a dog
+ * @name: pointer to name of dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Description: define the characteristics of a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* MAIN_H */
