b478919f4dde9e37a532195f24085630f08458f5
Amos-jaf <araunahamos@gmail.com>
2023-11-07 04:35:31 +0000

    free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+/**
+ * free_dog - function that frees dogs
+ * @d: input struct
+ * Description: function to deallocate memmory from dog struct
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+		free(d);
+}
