074c53a2c85942e9a4f10294284442c79d1b01e4
Amos-jaf <araunahamos@gmail.com>
2023-11-11 03:47:15 +0000

    print variable number of integers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function that prints numbers of variable arguments
+ * @separator: string or character that separates printed arguments
+ * @n: number of arguments
+ * ...: variable number of arguments
+ * Description: the function uses stdarg.h and its funtions to  print all
+ * arguments (intergers) passed into it
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list arg;
+
+	va_start(arg, n);
+
+	for (i = 0; i < n; i++)
+	{
+
+		printf("%d", va_arg(arg, int));
+		if (i < n - 1)
+		{
+			if (separator != NULL)
+			{
+				printf("%s", separator);
+			}
+		}
+	}
+	printf("\n");
+}
