c4a6276830e20dc4503c7b70a2652d502bf1efa3
Amos-jaf <araunahamos@gmail.com>
2023-11-27 13:26:19 +0000

    convert decimal to binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,6 +16,10 @@ unsigned int binary_to_uint(const char *b)
 	number = 0;
 	factor = 1;
 
+	if (b == NULL)
+	{
+		return (0);
+	}
 	a = copy_buffer(b);
 	a = swap_str(a);
 	for (i = 0; a[i] != '\0'; i++)
