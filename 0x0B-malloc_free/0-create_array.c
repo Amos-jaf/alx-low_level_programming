ec4462bb19251b598d0debd44175eb2c5d5a785f
Amos-jaf <araunahamos@gmail.com>
2024-01-16 10:43:42 +0000

    Dynamic memory allocation for string population using loop

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,31 +1,26 @@
 #include "main.h"
 
 /**
- * create_array - function to create an array and initialize it with a value
- * @size: size of the created array
- * @c: value that elements of the array are initialized with
- * Description: utilizes malloc function to initialize an array of size
- * "size" with a value of c
- * Return: pointer to the array
+ * create_array - function that passes a character into an array
+ * @size: Size of the buffer
+ * @c: character to be populated in the buffer
+ * Description: A buffer is created dynamically using the malloc function
+ * based on the size argument passed into the function
+ * Using a loop the character denoted by 'c' is populated into all the bytes
+ * of the buffer.
+ * Return: buffer string
  */
 
 char *create_array(unsigned int size, char c)
 {
-	char *buffer;
 	unsigned int i;
+	char *buffer;
 
-	buffer = NULL;
 	if (size == 0)
-	{
 		return (NULL);
-	}
-	buffer = malloc(sizeof(*buffer) * size);
-
+	buffer = malloc(sizeof(char) * size);
 	if (buffer == NULL)
-	{
 		return (NULL);
-	}
-
 	for (i = 0; i < size; i++)
 	{
 		buffer[i] = c;
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,1 +1,1 @@
-Readme for malloc projects
+Reade for malloc function
