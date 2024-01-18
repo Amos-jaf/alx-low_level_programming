03e4819934fb741fa5adc9e1923c267fa7e0c2ba
Amos Araunah <amosaraunah@gmail.com>
2024-01-18 23:42:53 +0100

    using malloc to simulate calloc function

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,18 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 
 	ptr = malloc(size * nmemb);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+	{
+		((char *)ptr)[i] = 0;
+	}
 	return (ptr);
 }
