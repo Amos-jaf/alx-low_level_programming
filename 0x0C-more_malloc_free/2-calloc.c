cdce59cce7b2ff8b0f93144d7efbda097710bd1e
Amos Araunah <amosaraunah@gmail.com>
2024-01-18 23:23:13 +0100

    using malloc to simulate calloc function

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,16 @@
 #include "main.h"
 
+/**
+ * _calloc - function that simulates the calloc function
+ * @nmemb: The number of array elements to be assigned memory
+ * @size: The size of each element in the array
+ *
+ * Description: Utilizes the malloc function to assign the
+ * specific amount of memmory that is enough for the array while
+ * taking into consideration the size of each element.
+ * Return: The size of the memory to be allocated
+ */
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
